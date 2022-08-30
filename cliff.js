edbf0e09d66c48a7219f1e5567ec4e4954b180c6
ombiro96 <bakutarb@gmail.com>
2022-08-30 21:46:01 +0300

    Added ??:??

---
+++ cliff.js
@@ -0,0 +1,14 @@
+// finding all possibilities of ??:??
+//Hh:Mm
+//
+//if H=? && h>3
+//hourCombinations=2
+//else if H=? && h<4
+//hourCombinations=3
+//if H=0|1 && h=='?'
+// hourCombinations=10
+//if H===2 && h=='?'
+//hourCombinations=4
+//else if H='?' && h='?'
+//hourCombinations=24
+//for later
